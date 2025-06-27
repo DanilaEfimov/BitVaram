083417a760b01e491a487ccd00df29f8baa95f62
Danila <efimovdanila.de@gmail.col>
2025-06-27 20:34:42 +0400

    Initial commit

---
+++ BitVaram/src/BitVaram.cpp
@@ -0,0 +1,12 @@
+﻿// BitVaram.cpp: defined entry point for app.
+//
+
+#include "BitVaram.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
