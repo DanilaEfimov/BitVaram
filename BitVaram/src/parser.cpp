b7ec783691614acc6f87b43d222e6cd178628fd3
Danila <efimovdanila.de@gmail.col>
2025-07-29 14:15:08 +0400

    refactoring + add system_call statemate

--- BitVaram/src/parser.cpp
+++ BitVaram/src/parser.cpp
@@ -8,14 +8,9 @@ using namespace compiler;
 
 
 // here would looks cool some statemate fabric
-Parser::StatemateType Parser::getType(const expression& expr) const {
+statemates::StatemateType Parser::getTypeImpl(const expression& expr) const {
     try {
-        if (statemates::identifier_decl::isValidSyntax(expr))
-            return StatemateType::Identifier_decl;
-        if (statemates::undeclaration::isValidSyntax(expr))
-            return StatemateType::Undeclaration;
-        if (statemates::keyword::isValidSyntax(expr))
-            return StatemateType::Keyword;
+        return statemates::getType(expr);
     }
     catch (std::exception& e) {
         this->context.addError(e.what(), expr[0].getPosition());
@@ -25,36 +20,9 @@ Parser::StatemateType Parser::getType(const expression& expr) const {
     }
 }
 
-int Parser::getBound(const std::vector<expression>& source, StatemateType type, int start) const {
+int Parser::getBoundImpl(const std::vector<expression>& source, statemates::StatemateType type, int start) const {
     try {
-        std::vector<expression> code = 
-            std::vector<expression>(source.begin() + start, source.end());
-        int end = start;
-        switch (type) {
-        case StatemateType::Identifier_decl: 
-            end += statemates::identifier_decl::getStatemateBound(code);
-            break;
-        case StatemateType::Undeclaration:
-            end += statemates::undeclaration::getStatemateBound(code);
-            break;
-        case StatemateType::Keyword:
-            end += statemates::keyword::getStatemateBound(code);
-            break;
-        case StatemateType::Block:
-            end += statemates::block::getStatemateBound(code);
-            break;
-        case StatemateType::Function_decl:
-        case StatemateType::Assignment:
-        case StatemateType::System_call:
-        default:
-        {
-            this->context.addError(varam::messages[invalidSyntax], source[0][0].getPosition());
-            this->context.setStatus(Status::ErrorOccurred);
-            return 0;
-        }
-        }
-
-        return end;
+        return statemates::getBound(source, type, start);
     }
     catch (std::exception& e) {
         this->context.addError(e.what(), source[0][0].getPosition());
@@ -64,43 +32,13 @@ int Parser::getBound(const std::vector<expression>& source, StatemateType type, 
     }
 }
 
-std::unique_ptr<statemates::statemate> Parser::makeStatemate(
+std::unique_ptr<statemates::statemate> Parser::makeStatemateImpl(
     statemates::block* parent,
-    StatemateType type,
-    std::vector<expression>& code
+    statemates::StatemateType type,
+    const std::vector<expression>& code
 ) const {
-
     try {
-        switch (type) {
-        case StatemateType::Identifier_decl:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::identifier_decl(parent, code));
-        case StatemateType::Undeclaration:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::undeclaration(parent, code));
-        case StatemateType::Keyword:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::keyword(parent, code));
-        case StatemateType::Block:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::block(parent));
-        case StatemateType::Function_decl:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::function_decl(parent));
-        case StatemateType::Assignment:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::assignment(parent));
-        case StatemateType::System_call:
-            return std::unique_ptr<statemates::statemate>
-                (new statemates::system_call(parent));
-        default:
-        {
-            this->context.addError(varam::messages[invalidSyntax], code[0][0].getPosition());
-            this->context.setSourceCode(code[0][0].getValue());
-            this->context.setStatus(Status::ErrorOccurred);
-            return nullptr;
-        }
-        }
+        return statemates::makeStatemate(parent, type, code);
     }
     catch (std::exception& e) {
         this->context.addError(e.what(), code[0][0].getPosition());
@@ -110,8 +48,8 @@ std::unique_ptr<statemates::statemate> Parser::makeStatemate(
     }
 }
 
-statemates::block* Parser::buildAST(
-    std::vector<expression>& code,
+statemates::block* Parser::buildASTImpl(
+    const std::vector<expression>& code,
     statemates::block* root
 ) const {
 
@@ -119,25 +57,7 @@ statemates::block* Parser::buildAST(
         return nullptr;
     }
 
-    for (int i = 0; i < static_cast<int>(code.size());) {
-        StatemateType type = this->getType(code[i]);
-        int start = i;
-        int end = this->getBound(code, type, i) + 1;
-
-        if (start >= end) {
-            throw std::runtime_error(varam::messages[undefinedError]);
-            break;
-        }
-        std::vector<expression> source = 
-            std::vector<expression>(code.begin() + start, code.begin() + end);
-
-        std::unique_ptr<statemates::statemate> child = this->makeStatemate(root, type, source);
-        root->instructions.push_back(std::move(child));
-
-        i = end;
-    }
-
-    return root;
+    return statemates::buildAST(code, root);
 }
 
 void Parser::occureSuccessState(varam::Config& config) const {
@@ -158,7 +78,6 @@ void Parser::occureSuccessState(varam::Config& config) const {
 Parser::Parser()
     : context(), ASTroot(new statemates::block())
 {
-    std::cout << boost::json::serialize(this->ASTroot->toJson()) << std::endl;
 }
 
 std::string Parser::replacedPrefixName(const std::string& filename) {
@@ -175,7 +94,7 @@ std::string Parser::replacedPrefixName(const std::string& filename) {
     return newName;
 }
 
-void Parser::process(varam::Config& config, std::vector<expression>& code) {
+void Parser::process(varam::Config& config, const std::vector<expression>& code) {
 
     this->context.setPhase(Phase::Parsing);
     this->context.setStatus(Status::Started);
@@ -183,7 +102,7 @@ void Parser::process(varam::Config& config, std::vector<expression>& code) {
     config.set("input", replacedPrefixName(config.getInputFile()));
 
     try {
-        this->ASTroot = this->buildAST(code, this->ASTroot);
+        this->ASTroot = this->buildASTImpl(code, this->ASTroot);
         this->occureSuccessState(config);
     }
     catch (std::runtime_error& e) {
@@ -193,6 +112,11 @@ void Parser::process(varam::Config& config, std::vector<expression>& code) {
     catch (...) {
         this->context.addError(varam::messages[undefinedError], Position());
         this->context.setStatus(Status::Aborted);
+    }
+
+    for (const auto& error : this->context.getErrors()) {
+        std::cerr << error.message << " at " 
+            << std::string(error.position) << std::endl;
     }
     
 }
